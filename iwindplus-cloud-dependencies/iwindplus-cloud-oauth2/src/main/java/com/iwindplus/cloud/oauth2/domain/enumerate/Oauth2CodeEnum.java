74dab3971dd0bdf5b265b92014b4c84e1aa84a7e
zengdegui <26548702+zengdegui@users.noreply.github.com>
2020-12-17 14:40:06 +0800

    Update Oauth2CodeEnum.java

--- iwindplus-cloud-dependencies/iwindplus-cloud-oauth2/src/main/java/com/iwindplus/cloud/oauth2/domain/enumerate/Oauth2CodeEnum.java
+++ iwindplus-cloud-dependencies/iwindplus-cloud-oauth2/src/main/java/com/iwindplus/cloud/oauth2/domain/enumerate/Oauth2CodeEnum.java
@@ -1,7 +1,6 @@
 /**
  * Copyright (c) iwindplus Technologies Co., Ltd.2011-2020, All rights reserved.
  */
-
 package com.iwindplus.cloud.oauth2.domain.enumerate;
 
 import lombok.Getter;
@@ -21,8 +20,14 @@ public enum Oauth2CodeEnum {
     SKEY_NOT_EXIST("skey_not_exist", "绑定key不存在"),
     ;
 
+    /**
+     * 值
+     */
     private final String value;
 
+    /**
+     * 描述
+     */
     private final String desc;
 
     private Oauth2CodeEnum(final String value, final String desc) {
@@ -30,6 +35,12 @@ public enum Oauth2CodeEnum {
         this.desc = desc;
     }
 
+    /**
+     * 通过描述查找枚举
+     *
+     * @param desc 描述
+     * @return Oauth2CodeEnum
+     */
     public static Oauth2CodeEnum valueOfDesc(String desc) {
         for (Oauth2CodeEnum val : Oauth2CodeEnum.values()) {
             if (Objects.equals(desc, val.desc())) {
@@ -39,6 +50,12 @@ public enum Oauth2CodeEnum {
         return null;
     }
 
+    /**
+     * 通过值查找枚举
+     *
+     * @param value 值
+     * @return Oauth2CodeEnum
+     */
     public static Oauth2CodeEnum valueOfValue(String value) {
         for (Oauth2CodeEnum val : Oauth2CodeEnum.values()) {
             if (Objects.equals(value, val.value())) {
