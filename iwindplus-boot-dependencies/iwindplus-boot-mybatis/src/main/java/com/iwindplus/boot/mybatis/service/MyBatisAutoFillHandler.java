3f95c030c11b839e8e927f2fdf7f1624b920b6a5
zengdegui <zengdegui@zzsim.com>
2021-01-11 19:54:06 +0800

    邮箱增加密送人

--- iwindplus-boot-dependencies/iwindplus-boot-mybatis/src/main/java/com/iwindplus/boot/mybatis/service/MyBatisAutoFillHandler.java
+++ iwindplus-boot-dependencies/iwindplus-boot-mybatis/src/main/java/com/iwindplus/boot/mybatis/service/MyBatisAutoFillHandler.java
@@ -31,131 +31,131 @@ import java.util.Map;
  */
 @Slf4j
 public class MyBatisAutoFillHandler implements MetaObjectHandler {
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    @Autowired
-    private HttpServletRequest request;
+	@Autowired
+	private HttpServletRequest request;
 
-    /**
-     * 添加填充.
-     *
-     * @param metaObject metaObject
-     */
-    @Override
-    public void insertFill(MetaObject metaObject) {
-        log.info("Mybatis auto fill when public fields are added");
-        if (null == metaObject.getValue(MybatisConstant.GMT_CREATE)) {
-            // 设置创建时间.
-            this.strictInsertFill(metaObject, MybatisConstant.GMT_CREATE, LocalDateTime.class, LocalDateTime.now());
-        }
-        if (null == metaObject.getValue(MybatisConstant.GMT_MODIFIED)) {
-            // 设置更新时间.
-            this.strictInsertFill(metaObject, MybatisConstant.GMT_MODIFIED, LocalDateTime.class, LocalDateTime.now());
-        }
-        if (null == metaObject.getValue(MybatisConstant.CREATER)) {
-            // 设置创建人.
-            String operator = this.getOperator();
-            if (StringUtils.isNotBlank(operator)) {
-                this.strictInsertFill(metaObject, MybatisConstant.CREATER, String.class, operator);
-            }
-        }
-        if (null == metaObject.getValue(MybatisConstant.HOST)) {
-            // 设置主机.
-            String host = NetUtil.getLocalhostStr();
-            if (StringUtils.isNotBlank(host)) {
-                this.strictInsertFill(metaObject, MybatisConstant.HOST, String.class, host);
-            }
-        }
-        // 设置是否删除.
-        this.strictInsertFill(metaObject, MybatisConstant.FLAG_DELETE, Boolean.class, false);
-        // 设置乐观锁.
-        this.strictInsertFill(metaObject, MybatisConstant.VERSION, Integer.class, 0);
-    }
+	/**
+	 * 添加填充.
+	 *
+	 * @param metaObject metaObject
+	 */
+	@Override
+	public void insertFill(MetaObject metaObject) {
+		log.info("Mybatis auto fill when public fields are added");
+		if (null == metaObject.getValue(MybatisConstant.GMT_CREATE)) {
+			// 设置创建时间.
+			this.strictInsertFill(metaObject, MybatisConstant.GMT_CREATE, LocalDateTime.class, LocalDateTime.now());
+		}
+		if (null == metaObject.getValue(MybatisConstant.GMT_MODIFIED)) {
+			// 设置更新时间.
+			this.strictInsertFill(metaObject, MybatisConstant.GMT_MODIFIED, LocalDateTime.class, LocalDateTime.now());
+		}
+		if (null == metaObject.getValue(MybatisConstant.CREATER)) {
+			// 设置创建人.
+			String operator = this.getOperator();
+			if (StringUtils.isNotBlank(operator)) {
+				this.strictInsertFill(metaObject, MybatisConstant.CREATER, String.class, operator);
+			}
+		}
+		if (null == metaObject.getValue(MybatisConstant.HOST)) {
+			// 设置主机.
+			String host = NetUtil.getLocalhostStr();
+			if (StringUtils.isNotBlank(host)) {
+				this.strictInsertFill(metaObject, MybatisConstant.HOST, String.class, host);
+			}
+		}
+		// 设置是否删除.
+		this.strictInsertFill(metaObject, MybatisConstant.FLAG_DELETE, Boolean.class, false);
+		// 设置乐观锁.
+		this.strictInsertFill(metaObject, MybatisConstant.VERSION, Integer.class, 0);
+	}
 
-    /**
-     * 更新填充.
-     *
-     * @param metaObject metaObject
-     */
-    @Override
-    public void updateFill(MetaObject metaObject) {
-        log.info("Mybatis auto fill when editing public fields");
-        if (null == metaObject.getValue(MybatisConstant.GMT_MODIFIED)) {
-            // 设置更新时间
-            this.strictUpdateFill(metaObject, MybatisConstant.GMT_MODIFIED, LocalDateTime.class, LocalDateTime.now());
-        }
-        if (null == metaObject.getValue(MybatisConstant.MODIFIER)) {
-            // 设置更新人
-            String operator = this.getOperator();
-            if (StringUtils.isNotBlank(operator)) {
-                this.strictUpdateFill(metaObject, MybatisConstant.MODIFIER, String.class, operator);
-            }
-        }
-    }
+	/**
+	 * 更新填充.
+	 *
+	 * @param metaObject metaObject
+	 */
+	@Override
+	public void updateFill(MetaObject metaObject) {
+		log.info("Mybatis auto fill when editing public fields");
+		if (null == metaObject.getValue(MybatisConstant.GMT_MODIFIED)) {
+			// 设置更新时间
+			this.strictUpdateFill(metaObject, MybatisConstant.GMT_MODIFIED, LocalDateTime.class, LocalDateTime.now());
+		}
+		if (null == metaObject.getValue(MybatisConstant.MODIFIER)) {
+			// 设置更新人
+			String operator = this.getOperator();
+			if (StringUtils.isNotBlank(operator)) {
+				this.strictUpdateFill(metaObject, MybatisConstant.MODIFIER, String.class, operator);
+			}
+		}
+	}
 
-    /**
-     * 获取操作人.
-     *
-     * @return String
-     */
-    public String getOperator() {
-        return this.getOperator2();
-    }
+	/**
+	 * 获取操作人.
+	 *
+	 * @return String
+	 */
+	public String getOperator() {
+		return this.getOperator2();
+	}
 
-    /**
-     * 获取操作人.
-     *
-     * @return String
-     */
-    private String getOperator2() {
-        if (null == this.request) {
-            return null;
-        }
-        try {
-            String operator = null;
-            XssHttpServletRequestWrapper requestWrapper = new XssHttpServletRequestWrapper(this.request);
-            // Cookie不为空返回
-            Cookie[] cookies = requestWrapper.getCookies();
-            for (Cookie cookie : cookies) {
-                if (StringUtils.equals(MybatisConstant.OPERATOR, cookie.getName())) {
-                    operator = cookie.getValue();
-                    return operator;
-                }
-            }
-            // 请求头不为空返回
-            operator = requestWrapper.getHeader(MybatisConstant.OPERATOR);
-            if (StringUtils.isNotBlank(operator)) {
-                return operator;
-            }
-            // 请求参数不为空返回
-            operator = requestWrapper.getParameter(MybatisConstant.OPERATOR);
-            if (StringUtils.isNotBlank(operator)) {
-                return operator;
-            }
-            // 请求body体不为空返回
-            byte[] body = requestWrapper.getBody();
-            if (ArrayUtils.isNotEmpty(body)) {
-                return this.getOperatorByBody(body);
-            }
-        } catch (Exception e) {
-            log.error("XssHttpServletRequestWrapper Exception [{}]", e);
-        }
-        return null;
-    }
+	/**
+	 * 获取操作人.
+	 *
+	 * @return String
+	 */
+	private String getOperator2() {
+		if (null == this.request) {
+			return null;
+		}
+		try {
+			String operator = null;
+			XssHttpServletRequestWrapper requestWrapper = new XssHttpServletRequestWrapper(this.request);
+			// Cookie不为空返回
+			Cookie[] cookies = requestWrapper.getCookies();
+			for (Cookie cookie : cookies) {
+				if (StringUtils.equals(MybatisConstant.OPERATOR, cookie.getName())) {
+					operator = cookie.getValue();
+					return operator;
+				}
+			}
+			// 请求头不为空返回
+			operator = requestWrapper.getHeader(MybatisConstant.OPERATOR);
+			if (StringUtils.isNotBlank(operator)) {
+				return operator;
+			}
+			// 请求参数不为空返回
+			operator = requestWrapper.getParameter(MybatisConstant.OPERATOR);
+			if (StringUtils.isNotBlank(operator)) {
+				return operator;
+			}
+			// 请求body体不为空返回
+			byte[] body = requestWrapper.getBody();
+			if (ArrayUtils.isNotEmpty(body)) {
+				return this.getOperatorByBody(body);
+			}
+		} catch (Exception e) {
+			log.error("XssHttpServletRequestWrapper Exception [{}]", e);
+		}
+		return null;
+	}
 
-    private String getOperatorByBody(byte[] body) {
-        try {
-            Map<?, ?> map = this.objectMapper.readValue(new String(body, Charset.defaultCharset()), Map.class);
-            if (MapUtils.isNotEmpty(map)) {
-                Object operator = map.get(MybatisConstant.OPERATOR);
-                if (null != operator) {
-                    return operator.toString();
-                }
-            }
-        } catch (JsonProcessingException ex) {
-            log.error("Json processing exception [{}]", ex);
-        }
-        return null;
-    }
+	private String getOperatorByBody(byte[] body) {
+		try {
+			Map<?, ?> map = this.objectMapper.readValue(new String(body, Charset.defaultCharset()), Map.class);
+			if (MapUtils.isNotEmpty(map)) {
+				Object operator = map.get(MybatisConstant.OPERATOR);
+				if (null != operator) {
+					return operator.toString();
+				}
+			}
+		} catch (JsonProcessingException ex) {
+			log.error("Json processing exception [{}]", ex);
+		}
+		return null;
+	}
 }
