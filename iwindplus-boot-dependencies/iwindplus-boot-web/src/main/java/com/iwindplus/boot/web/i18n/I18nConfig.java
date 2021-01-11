811cbae93940cc15d5a8503b59da27a37af6e9f2
zengdegui <zengdegui@zzsim.com>
2021-01-11 19:23:32 +0800

    邮箱增加密送人

--- iwindplus-boot-dependencies/iwindplus-boot-web/src/main/java/com/iwindplus/boot/web/i18n/I18nConfig.java
+++ iwindplus-boot-dependencies/iwindplus-boot-web/src/main/java/com/iwindplus/boot/web/i18n/I18nConfig.java
@@ -23,43 +23,43 @@ import java.util.Properties;
  */
 @Configuration
 public class I18nConfig {
-    @Autowired
-    private MessageSource messageSource;
+	@Autowired
+	private MessageSource messageSource;
 
-    /**
-     * 统一校验规则到国际化信息文件.
-     *
-     * @return Validator
-     */
-    @Primary
-    @Bean
-    public Validator getValidator() {
-        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
-        validator.setValidationMessageSource(this.messageSource);
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.validator.fail_fast", "true");
-        validator.setValidationProperties(properties);
-        return validator;
-    }
+	/**
+	 * 统一校验规则到国际化信息文件.
+	 *
+	 * @return Validator
+	 */
+	@Primary
+	@Bean
+	public Validator getValidator() {
+		LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(this.messageSource);
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.validator.fail_fast", "true");
+		validator.setValidationProperties(properties);
+		return validator;
+	}
 
-    /**
-     * 根据code获取，没有则取默认消息.
-     *
-     * @param code 编码
-     * @return String
-     */
-    public String getMessage(String code) {
-        return this.getMessageByOS(code, null);
-    }
+	/**
+	 * 根据code获取，没有则取默认消息.
+	 *
+	 * @param code 编码
+	 * @return String
+	 */
+	public String getMessage(String code) {
+		return this.getMessageByOS(code, null);
+	}
 
-    /**
-     * 根据应用部署的服务器系统来决定国际化.
-     *
-     * @param code 编码
-     * @param defaultMessage 默认消息
-     * @return String
-     */
-    public String getMessageByOS(String code, String defaultMessage) {
-        return this.messageSource.getMessage(code, null, defaultMessage, LocaleContextHolder.getLocale());
-    }
+	/**
+	 * 根据应用部署的服务器系统来决定国际化.
+	 *
+	 * @param code           编码
+	 * @param defaultMessage 默认消息
+	 * @return String
+	 */
+	public String getMessageByOS(String code, String defaultMessage) {
+		return this.messageSource.getMessage(code, null, defaultMessage, LocaleContextHolder.getLocale());
+	}
 }
